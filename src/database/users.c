3002ff4dece73d5397013a11b130dc7c32f9346d
sankalpdevtester <ohofufic0@gmail.com>
2026-06-18 17:54:45 +0530

    feat: add user database functionality

--- src/database/users.c
+++ src/database/users.c
@@ -2,79 +2,56 @@
 #include "src/database/models.c"
 
 // Function to create users table
-void create_users_table() {
-    sqlite3 *db;
-    char *zErrMsg = 0;
-    int rc = sqlite3_open("database.db", &db);
-    if (rc) {
-        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-        return;
-    }
-
-    char *sql = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)";
-    rc = sqlite3_exec(db, sql, NULL, NULL, &zErrMsg);
+void create_users_table(sqlite3 *db) {
+    const char *query = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)";
+    char *errMsg = NULL;
+    int rc = sqlite3_exec(db, query, NULL, NULL, &errMsg);
     if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
+        fprintf(stderr, "Error creating users table: %s\n", errMsg);
+        sqlite3_free(errMsg);
     }
-    sqlite3_close(db);
 }
 
-// Function to insert user into database
-void insert_user(const char *username, const char *password) {
-    sqlite3 *db;
-    char *zErrMsg = 0;
-    int rc = sqlite3_open("database.db", &db);
-    if (rc) {
-        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+// Function to get user by username
+void get_user_by_username(sqlite3 *db, const char *username, User **user) {
+    sqlite3_stmt *stmt;
+    const char *query = "SELECT * FROM users WHERE username = ?";
+    int rc = sqlite3_prepare_v2(db, query, -1, &stmt, 0);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Error preparing query: %s\n", sqlite3_errmsg(db));
         return;
     }
 
-    char *sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-    sqlite3_stmt *stmt;
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
+    sqlite3_bind_text(stmt, 1, username, -1, SQLITE_STATIC);
+    rc = sqlite3_step(stmt);
+    if (rc == SQLITE_ROW) {
+        *user = malloc(sizeof(User));
+        (*user)->id = sqlite3_column_int(stmt, 0);
+        (*user)->username = strdup(sqlite3_column_text(stmt, 1));
+        (*user)->password = strdup(sqlite3_column_text(stmt, 2));
     } else {
-        sqlite3_bind_text(stmt, 1, username, -1, SQLITE_STATIC);
-        sqlite3_bind_text(stmt, 2, password, -1, SQLITE_STATIC);
-        rc = sqlite3_step(stmt);
-        if (rc != SQLITE_DONE) {
-            fprintf(stderr, "SQL error: %s\n", zErrMsg);
-            sqlite3_free(zErrMsg);
-        }
+        *user = NULL;
     }
+
     sqlite3_finalize(stmt);
-    sqlite3_close(db);
 }
 
-// Function to query user from database
-void query_user(const char *username, const char *password) {
-    sqlite3 *db;
-    char *zErrMsg = 0;
-    int rc = sqlite3_open("database.db", &db);
-    if (rc) {
-        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+// Function to add user
+void add_user(sqlite3 *db, const char *username, const char *password) {
+    sqlite3_stmt *stmt;
+    const char *query = "INSERT INTO users (username, password) VALUES (?, ?)";
+    int rc = sqlite3_prepare_v2(db, query, -1, &stmt, 0);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Error preparing query: %s\n", sqlite3_errmsg(db));
         return;
     }
 
-    char *sql = "SELECT * FROM users WHERE username = ? AND password = ?";
-    sqlite3_stmt *stmt;
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-    } else {
-        sqlite3_bind_text(stmt, 1, username, -1, SQLITE_STATIC);
-        sqlite3_bind_text(stmt, 2, password, -1, SQLITE_STATIC);
-        rc = sqlite3_step(stmt);
-        if (rc == SQLITE_ROW) {
-            // User found
-        } else {
-            // User not found
-        }
+    sqlite3_bind_text(stmt, 1, username, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, password, -1, SQLITE_STATIC);
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_DONE) {
+        fprintf(stderr, "Error adding user: %s\n", sqlite3_errmsg(db));
     }
+
     sqlite3_finalize(stmt);
-    sqlite3_close(db);
 }
