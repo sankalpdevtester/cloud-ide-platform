73ac91a38d00feef190c0facf815bb6233253064
sankalpdevtester <ohofufic0@gmail.com>
2026-06-18 17:54:49 +0530

    feat: add authentication API

--- src/api/auth_api.c
+++ src/api/auth_api.c
@@ -1,56 +1,48 @@
 #include <curl/curl.h>
 #include "src/config/config.c"
 
-// Function to handle user registration API request
+// Function to handle login API request
 static size_t write_callback(void *ptr, size_t size, size_t nmemb, void *userdata) {
     return size * nmemb;
 }
 
-void register_user_api(const char *username, const char *password) {
+void login_api(const char *username, const char *password) {
     CURL *curl;
     CURLcode res;
     curl_global_init(CURL_GLOBAL_DEFAULT);
     curl = curl_easy_init();
     if(curl) {
-        char *url = "http://localhost:8080/register";
+        char *url = malloc(strlen("https://example.com/login") + strlen(username) + strlen(password) + 1);
+        sprintf(url, "https://example.com/login?username=%s&password=%s", username, password);
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-        struct curl_slist *headers = NULL;
-        headers = curl_slist_append(headers, "Content-Type: application/json");
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-        char *data = malloc(strlen(username) + strlen(password) + 20);
-        sprintf(data, "{\"username\":\"%s\",\"password\":\"%s\"}", username, password);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
         res = curl_easy_perform(curl);
         if(res != CURLE_OK) {
             fprintf(stderr, "cURL error: %s\n", curl_easy_strerror(res));
         }
         curl_easy_cleanup(curl);
+        free(url);
     }
     curl_global_cleanup();
 }
 
-// Function to handle user login API request
-void login_user_api(const char *username, const char *password) {
+// Function to handle register API request
+void register_api(const char *username, const char *password) {
     CURL *curl;
     CURLcode res;
     curl_global_init(CURL_GLOBAL_DEFAULT);
     curl = curl_easy_init();
     if(curl) {
-        char *url = "http://localhost:8080/login";
+        char *url = malloc(strlen("https://example.com/register") + strlen(username) + strlen(password) + 1);
+        sprintf(url, "https://example.com/register?username=%s&password=%s", username, password);
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-        struct curl_slist *headers = NULL;
-        headers = curl_slist_append(headers, "Content-Type: application/json");
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-        char *data = malloc(strlen(username) + strlen(password) + 20);
-        sprintf(data, "{\"username\":\"%s\",\"password\":\"%s\"}", username, password);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
         res = curl_easy_perform(curl);
         if(res != CURLE_OK) {
             fprintf(stderr, "cURL error: %s\n", curl_easy_strerror(res));
         }
         curl_easy_cleanup(curl);
+        free(url);
     }
     curl_global_cleanup();
 }
